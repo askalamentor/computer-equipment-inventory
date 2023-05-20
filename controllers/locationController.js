f99514d9ca1b15ff2c9f67998ee441425237f056
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-05-20 21:34:40 +0300

    Add create_location

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,6 +1,8 @@
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
 const Location = require('../models/location');
 const Inventory = require('../models/inventory');
-const asyncHandler = require('express-async-handler');
 
 // Display list of all Locations
 exports.location_list = asyncHandler(async (req, res, next) => {
@@ -37,13 +39,54 @@ exports.location_detail = asyncHandler(async (req, res, next) => {
 
 // Display Location create form on GET.
 exports.location_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Location create GET');
+  res.render('location/location_form', {
+    title: 'Create Location',
+  });
 });
 
 // Handle Location create on POST.
-exports.location_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Location create POST');
-});
+exports.location_create_post = [
+  // Validate and sanitize fields.
+  body('name', 'Name must not be empty.')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Name must be at least 3 characters')
+    .isLength({ max: 100 })
+    .withMessage('Name must not exceed 100 characters')
+    .escape(),
+  body('address', 'Address must not be empty.')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Address must e at least 3 characters')
+    .isLength({ max: 200 })
+    .withMessage('Address must not exceed 200 characters')
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Location object with escaped and trimmed data.
+    const location = new Location({
+      name: req.body.name,
+      address: req.body.address,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      res.render('location/location_form', {
+        title: 'Create Location',
+        location: location,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from valid is valid.
+      await location.save();
+      res.redirect(location.url);
+    }
+  }),
+];
 
 // Display Location delete form on GET
 exports.location_delete_get = asyncHandler(async (req, res, next) => {
